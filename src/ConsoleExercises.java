d953fdb845974316c461d5549ed01d3b783e0fda
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-30 15:41:28 -0500

    console exercises complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,7 +4,7 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         int userInt;
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
 
         System.out.print("Please enter an integer: ");
         userInt = sc.nextInt();
@@ -21,6 +21,24 @@ public class ConsoleExercises {
         System.out.println("Your second word is: " + secondWord);
         thirdWord = sc.next();
         System.out.println("Your third word is: " + thirdWord);
+
+//        String sentenceInput;
+//        System.out.print("Please enter a sentence: ");
+//        sentenceInput = sc.next();
+//        System.out.println("Here is my sentence: " + sentenceInput);
+
+        String length;
+        String width;
+        System.out.print("Please enter width: ");
+        width = sc.next();
+        System.out.println(width);
+        int widthValue = Integer.parseInt(width);
+        System.out.print("Please enter length: ");
+        length = sc.next();
+        System.out.println(length);
+        int lengthValue = Integer.parseInt(length);
+        int area = lengthValue * widthValue;
+        System.out.println("Area is: " + area);
 
     }
 }

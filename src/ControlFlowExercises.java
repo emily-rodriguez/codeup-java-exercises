5e4c52e57d11a3783e8cc7681cbfbb3baac03047
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-01 10:28:35 -0500

    exercise: control flow exercise, complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("While loop: ");
 
         int i = 5;
@@ -35,16 +39,51 @@ public class ControlFlowExercises {
         System.out.println("FizzBuzz: ");
 
         for (int b = 1; b <= 100; b++) {
-            if (b % 3 == 0) {
-                System.out.println("Fizz");
-            } if (b % 5 == 0) {
-                System.out.println("Buzz");
-            } if ((b % 15 == 0)) {
+            if (b % 15 == 0) {
                 System.out.println("FizzBuzz");
+            } else if (b % 3 == 0){
+                System.out.println("Fizz");
+            } else if (b % 5 == 0) {
+                System.out.println("Buzz");
             } else {
-                System.out.println(b);
+                System.out.println(String.valueOf(b));
             }
         }
 
+        System.out.println("What number would you like to go up to? ");
+        int tableLength = scan.nextInt();
+
+        System.out.println("Would you like to continue? [y/N] ");
+        String userContinue = scan.next();
+
+        boolean confirmation = userContinue.equals("y");
+
+        System.out.println();
+        System.out.println(String.format("%6s| %7s |%6s","number","squared","cubed"));
+        System.out.println(String.format("%6s| %7s |%6s","-----","-----","-----"));
+
+        for (int c = 1; c <= tableLength; c++) {
+            System.out.println(String.format("%6s| %7s |%6s", c , c*c , c*c*c));
+        }
+
+        System.out.println("Please enter a grade from 0 to 100: ");
+        int gradeInput = scan.nextInt();
+
+        System.out.println("Would you like to continue? [y/N] ");
+        String gradeContinue = scan.next();
+
+        boolean gradeConfirmation = userContinue.equals("y");
+
+        if (gradeInput > 0 && gradeInput <= 59) {
+            System.out.println("Your grade is: F");
+        } else if (gradeInput >= 60 && gradeInput <= 66) {
+            System.out.println("Your grade is: D");
+        } else if (gradeInput >= 67 && gradeInput <= 79) {
+            System.out.println("Your grade is: C");
+        } else if (gradeInput >= 80 && gradeInput <= 87) {
+            System.out.println("Your grade is: B");
+        } else if (gradeInput >= 88 && gradeInput <= 100) {
+            System.out.println("Your grade is: A");
+        }
     }
 }

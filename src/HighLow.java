33b6e96ff9567433a498dd9f0e7a9710fa4f4b31
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-02 15:01:09 -0500

    exercise: highlow exercise, complete

---
+++ src/HighLow.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighLow {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Would you like to play a game? [y/N]");
+        String userInput = scan.next();
+
+
+        do {
+            int randomNumber = randomNumber(1, 100);
+            System.out.println(randomNumber);
+
+            System.out.print("Please guess a number between 1 and 100: ");
+            int userGuess = scan.nextInt();
+
+            guessValidation(userGuess, randomNumber);
+
+            System.out.print("Would you like to play again [y/N]");
+            userInput = scan.next();
+
+        } while (userInput.equals("y"));
+
+    }
+
+    public static int guessValidation(int guess, int random) {
+
+        Scanner sc = new Scanner(System.in);
+        int newGuess;
+        if (guess < random) {
+            System.out.print("HIGHER: ");
+            newGuess = sc.nextInt();
+            return guessValidation(newGuess, random);
+        }
+        if (guess > random) {
+            System.out.print("LOWER: ");
+            newGuess = sc.nextInt();
+            return guessValidation(newGuess, random);
+        }
+        if (guess == random) {
+            System.out.println("Good guess!");
+        }
+        return random;
+    }
+
+    public static int randomNumber (int min, int max) {
+        Random rand = new Random();
+        int randomNumberValue = rand.nextInt(max) + min;
+        return randomNumberValue;
+    }
+
+}

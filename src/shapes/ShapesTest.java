d40c80f89e71b7bc31c0f77bfa2fff446dcbc4bc
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-08 11:54:59 -0500

    exercise: inheritance and polymorphism, exercise complete

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,14 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(5, 4);
+        Rectangle box2 = new Square(5);
+
+        System.out.println("The area is: " + box1.getArea());
+        System.out.println("The perimeter is: " + box1.getPerimeter());
+
+        System.out.println("The area is: " + box2.getArea());
+        System.out.println("The perimeter is: " + box2.getPerimeter());
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,20 @@
+package shapes;
+
+public class Square extends Rectangle{
+    protected double side;
+
+    public Square(double side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+}

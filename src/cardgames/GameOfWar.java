5cf499c7da9fe33e6e0cf84604b7c5c96c8be7b6
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-09 11:05:16 -0500

    Bonus: Game of War card game complete

--- src/cardgames/GameOfWar.java
+++ src/cardgames/GameOfWar.java
@@ -2,11 +2,8 @@ package cardgames;
 
 import util.Input;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
-import static cardgames.Card.getCard;
 import static cardgames.Card.getCardName;
 
 public class GameOfWar {
@@ -14,7 +11,10 @@ public class GameOfWar {
     public static Card[] cards = CardsArray.getAllCards();
     public static Player player1;
     public static Player player2 = new Player("Computer");
-    public static int gameCount = 0;
+    public static Card player1Card;
+    public static Card player2Card;
+    public static int i;
+    public static int cardsInLimbo =0;
 
     public static Card[] shuffleDeck(Card[] cards){
         Random shuffle = new Random();
@@ -29,34 +29,70 @@ public class GameOfWar {
         return cards;
     }
 
-    public static void playGame() {
+    public static void warFaceoff(){
+        System.out.println("WAR!");
+        player1Card = cards[i += 2];
+        player2Card = cards[i += 2];
+        System.out.println(player1.name + "'s card: " + getCardName(player1Card) + " AND " + player2.name + "'s card: " + getCardName(player2Card));
+        if (player1Card.value > player2Card.value) {
+            System.out.println("Points to " + player1.name);
+            player1.playerScore += 6;
+            player1.playerScore += cardsInLimbo;
+        } else if (player2Card.value > player1Card.value) {
+            System.out.println("Points to " + player2.name);
+            player2.playerScore += 6;
+            player2.playerScore += cardsInLimbo;
+        } else if (player1Card.value == player2Card.value){
+            cardsInLimbo += 4;
+            warFaceoff();
+        }
+    }
 
+    public static void playGame() {
         shuffleDeck(cards);
 
         player1 = new Player(input.getString("Welcome player one! Please enter a user name: "));
 
-        for (int i = 0; i < cards.length - 1; i++) {
-            Card player1Card = cards[i];
-            Card player2Card = cards[i+=1];
+        for (i = 0; i < cards.length; i++) {
+            player1Card = cards[i];
+            player2Card = cards[i+=1];
             System.out.println(player1.name + "'s card: " + getCardName(player1Card) + " AND " + player2.name + "'s card: " + getCardName(player2Card));
 
             if (player1Card.value > player2Card.value) {
                 System.out.println("Point to " + player1.name);
-                player1.playerScore =+ 1;
+                player1.playerScore += 2;
             } else if (player2Card.value > player1Card.value) {
                 System.out.println("Point to " + player2.name);
-                player2.playerScore =+ 1;
+                player2.playerScore += 2;
             } else if (player2Card.value == player1Card.value) {
-                System.out.println("WAR!");
+                if (i >= 46) {
+                    System.out.println("Not enough cards for WAR.");
+                    player1.playerScore +=1;
+                    player2.playerScore +=1;
+                } else {
+                    warFaceoff();
+                }
             }
         }
 
+        System.out.println(player1.name + "'s score: " + player1.playerScore);
+        System.out.println(player2.name + "'s score: " + player2.playerScore);
 
-
-
-
+        if (player1.playerScore > player2.playerScore) {
+            System.out.println("You win " + player1.name + "!");
+        } else if (player2.playerScore > player1.playerScore){
+            System.out.println("You win " + player2.name + "!");
+        } else {
+            System.out.println("It's a tie!");
+        }
+        System.out.println("___$$_____$$______$$$_________$$$_____$$$$$$$_______$$$____$$_____$$__");
+        System.out.println("___$$_____$$____$$___$$_____$$___$$___$$_____$$____$___$____$$___$$___");
+        System.out.println("___$$_____$$___$$_____$$___$$_____$$__$$_____$$___$$___$$____$$_$$____");
+        System.out.println("___$$$$$$$$$___$$_____$$___$$_____$$__$$$$$$$____$$$$$$$$$_____$$_____");
+        System.out.println("___$$_____$$___$$_____$$___$$_____$$__$$___$$____$$_____$$_____$$_____");
+        System.out.println("___$$_____$$____$$___$$_____$$___$$___$$____$$___$$_____$$_____$$_____");
+        System.out.println("___$$_____$$______$$$_________$$$_____$$_____$$__$$_____$$_____$$_____");
     }
-
 
     public static void main(String[] args) {
         playGame();

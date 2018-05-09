c21089ce93d7ff6607c5990ed90ed743cd13bb02
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-09 16:06:13 -0500

    added trophy picture

--- src/cardgames/GameOfWar.java
+++ src/cardgames/GameOfWar.java
@@ -10,7 +10,7 @@ public class GameOfWar {
     public static Input input = new Input();
     public static Card[] cards = CardsArray.getAllCards();
     public static Player player1;
-    public static Player player2 = new Player("Computer");
+    public static Player player2;
     public static Card player1Card;
     public static Card player2Card;
     public static int i;
@@ -39,12 +39,12 @@ public class GameOfWar {
         player2Card = cards[i += 2];
         System.out.println(player1.name + "'s card: " + getCardName(player1Card) + " AND " + player2.name + "'s card: " + getCardName(player2Card));
         if (player1Card.value > player2Card.value) {
-            System.out.println("Points to " + player1.name);
+            System.out.println("War points to " + player1.name);
             player1.playerScore += 6;
             player1.playerScore += cardsInLimbo;
             System.out.println(player1.getScore(player1,player2));
         } else if (player2Card.value > player1Card.value) {
-            System.out.println("Points to " + player2.name);
+            System.out.println("War points to " + player2.name);
             player2.playerScore += 6;
             player2.playerScore += cardsInLimbo;
             System.out.println(player1.getScore(player1,player2));
@@ -58,6 +58,7 @@ public class GameOfWar {
         shuffleDeck(cards);
 
         player1 = new Player(input.getString("Welcome player one! Please enter a user name: "));
+        player2 = new Player(input.getString("Welcome player two! Please enter a user name: "));
 
         for (i = 0; i < cards.length; i++) {
             player1Card = cards[i];
@@ -65,11 +66,11 @@ public class GameOfWar {
             System.out.println(player1.name + "'s card: " + getCardName(player1Card) + " AND " + player2.name + "'s card: " + getCardName(player2Card));
 
             if (player1Card.value > player2Card.value) {
-                System.out.println("Point to " + player1.name);
+                System.out.println("2 points to " + player1.name);
                 player1.playerScore += 2;
                 System.out.println(player1.getScore(player1,player2));
             } else if (player2Card.value > player1Card.value) {
-                System.out.println("Point to " + player2.name);
+                System.out.println("2 points to " + player2.name);
                 player2.playerScore += 2;
                 System.out.println(player1.getScore(player1,player2));
             } else if (player2Card.value == player1Card.value) {
@@ -94,13 +95,16 @@ public class GameOfWar {
         } else {
             System.out.println("It's a tie!");
         }
-        System.out.println("___$$_____$$______$$$_________$$$_____$$$$$$$_______$$$____$$_____$$__");
-        System.out.println("___$$_____$$____$$___$$_____$$___$$___$$_____$$____$___$____$$___$$___");
-        System.out.println("___$$_____$$___$$_____$$___$$_____$$__$$_____$$___$$___$$____$$_$$____");
-        System.out.println("___$$$$$$$$$___$$_____$$___$$_____$$__$$$$$$$____$$$$$$$$$_____$$_____");
-        System.out.println("___$$_____$$___$$_____$$___$$_____$$__$$___$$____$$_____$$_____$$_____");
-        System.out.println("___$$_____$$____$$___$$_____$$___$$___$$____$$___$$_____$$_____$$_____");
-        System.out.println("___$$_____$$______$$$_________$$$_____$$_____$$__$$_____$$_____$$_____");
+        System.out.println("             ___________");
+        System.out.println("            '._==_==_=_.'");
+        System.out.println("            .-\\       /-.");
+        System.out.println("           | (|  .=.  |) |");
+        System.out.println("            '-| {{1}} |-'");
+        System.out.println("              \\  /|\\  /");
+        System.out.println("               '.   .'");
+        System.out.println("                 ) (");
+        System.out.println("               _.' '._");
+        System.out.println("              `\"\"\"\"\"\"\"`");
     }
 
 }
